718baf7d3ee368dc9bd96f73631444f686fcbe65
dhope1 <davidhopekavuma@gmail.com>
2025-06-22 00:08:46 +0300

    App.js Updated

--- archetype-quiz/src/App.js
+++ archetype-quiz/src/App.js
@@ -1,32 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+```js
 import React from 'react';
+import Quiz from './components/Quiz';
 import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="bg-gray-900 text-white p-4">
-      <h1 className="text-3xl font-bold text-blue-600">Testing Tailwind</h1>
-    </div>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Quiz />;
 }
 
 export default App;
-
-
+```
